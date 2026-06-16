70f704d12f3ac6867ca5ce70a181301333717456
joinseong96 <issseong9460163@gmail.com>
2026-06-16 18:04:32 +0900

    Badge components 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Button from "./components/Button/Button";
+import Badge from "./components/Badge/Badge";
 
 export default function App() {
 	return (
-		<div className="p-10 flex flex-col gap-4">
+		<div className="p-10 flex flex-col gap-8">
+			{/* Button */}
 			<div className="flex gap-3">
 				<Button variant="primary">Primary</Button>
 				<Button variant="secondary">Secondary</Button>
@@ -10,14 +12,13 @@ export default function App() {
 				<Button variant="danger">Danger</Button>
 			</div>
 
+			{/* Badge */}
 			<div className="flex gap-3 items-center">
-				<Button size="sm">Small</Button>
-				<Button size="md">Medium</Button>
-				<Button size="lg">Large</Button>
-			</div>
-
-			<div className="flex gap-3">
-				<Button disabled>Disabled</Button>
+				<Badge variant="primary">Primary</Badge>
+				<Badge variant="secondary">Secondary</Badge>
+				<Badge variant="success">Success</Badge>
+				<Badge variant="warning">Warning</Badge>
+				<Badge variant="danger">Danger</Badge>
 			</div>
 		</div>
 	);
