ca79fb297f74b2c41f9535e8f2df7cfd01843727
joinseong96 <issseong9460163@gmail.com>
2026-07-01 17:16:58 +0900

    sidebar button 수정

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -14,9 +14,9 @@ export default function ThemeToggle() {
 	return (
 		<button
 			onClick={() => setIsDark((prev) => !prev)}
-			className="w-full px-3 py-2 rounded text-xl bg-accent-700 font-medium text-gray-100 dark:text-gray-300 hover:bg-gray-800 dark:hover:bg-gray-800 transition-colors cursor-pointer"
+			className="w-full px-3 py-2 rounded text-xl bg-accent-700 dark:bg-accent-500 font-medium text-gray-100 dark:text-gray-300 hover:bg-gray-800 dark:hover:bg-gray-800 transition-colors cursor-pointer"
 		>
-			{isDark ? "Dark" : "Light"}
+			{isDark ? "Light" : "Dark"}
 		</button>
 	);
 }
